853d6b0d6207c243b0b8ab2f5fe8491001063bdd
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-08-25 14:47:03 +0200

    First version

---
+++ Thing.pm
@@ -0,0 +1,82 @@
+package Data::OFN::Thing;
+
+use strict;
+use warnings;
+
+use Mo qw(build default is);
+use Mo::utils qw(check_array_object check_isa);
+
+our $VERSION = 0.01;
+
+has attachment => (
+	default => [],
+	is => 'ro',
+);
+
+has created => (
+	is => 'ro',
+);
+
+has description => (
+	default => [],
+	is => 'ro',
+);
+
+has invalidated => (
+	is => 'ro',
+);
+
+has id => (
+	is => 'ro',
+);
+
+has iri => (
+	is => 'ro',
+);
+
+has name => (
+	default => [],
+	is => 'ro',
+);
+
+has relevant_to => (
+	is => 'ro',
+);
+
+has updated => (
+	is => 'ro',
+);
+
+sub BUILD {
+	my $self = shift;
+
+	# Check attachment.
+	# TODO
+
+	# Check created.
+	check_isa($self, 'created', 'Data::OFN::Common::TimeMoment');
+
+	# Check invalidated.
+	check_isa($self, 'invalidated', 'Data::OFN::Common::TimeMoment');
+
+	# Check id.
+	check_number($self, 'id');
+
+	# Check iri.
+	# TODO
+
+	# Check name.
+	check_array_object($self, 'name', 'Data::Text::Simple', 'Name');
+
+	# Check relevant_to.
+	check_isa($self, 'relevant_to', 'Data::OFN::Common::TimeMoment');
+
+	# Check updated.
+	check_isa($self, 'updated', 'Data::OFN::Common::TimeMoment');
+
+	return;
+}
+
+1;
+
+__END__
